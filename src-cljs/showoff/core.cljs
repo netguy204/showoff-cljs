d1ad6d2b1cae5644ac0332dffbd526b8cfa99257
Brian Taylor <el.wubo@gmail.com>
2012-04-14 09:37:58 -0400

    hud, smooth scroll, nearest neighbor

--- src-cljs/showoff/core.cljs
+++ src-cljs/showoff/core.cljs
@@ -17,14 +17,21 @@
 (def ^:dynamic *viewport* nil) ;; expressed in tiles
 (def ^:dynamic *world-dims* [640 480]) ;; expressed in pixels
 (def ^:dynamic *tile-dims* [8 8])
+(def *tile-in-world-dims* [(/ 640 16) (/ 480 12)])
+
 (def *default-color* [255 0 255])
 
+(defn make-canvas [[w h]]
+  (let [canvas (dom/createDom "canvas")]
+    (set! (.-width canvas) w)
+    (set! (.-height canvas) h)
+    canvas))
+
 (defn prepare-display []
-  (set! *display* (dom/createDom "canvas"))
-  (set! *viewport* [0 0 16 12])
-  (set! (.-width *display*) (nth *world-dims* 0))
-  (set! (.-height *display*) (nth *world-dims* 1))
-  (dom/appendChild (content) *display*))
+  (let [[w h] *world-dims*]
+    (set! *display* (make-canvas [w h]))
+    (set! *viewport* [0 0 16 12])
+    (dom/appendChild (content) *display*)))
 
 (defn transform [[x y w h]]
   "convert from x y w h in tilespace to a position in visible screen
@@ -53,7 +60,9 @@ space taking into account the current viewport"
 
 (defn context
   ([] (context *display*))
-  ([canvas] (.getContext canvas "2d")))
+  ([canvas] (let [ctx (.getContext canvas "2d")]
+              (set! (.-imageSmoothingEnabled ctx) false)
+              ctx)))
 
 (defn clear
   ([] (clear *display*))
@@ -65,29 +74,47 @@ space taking into account the current viewport"
 
 (def format string/format)
 
-(defn color [[r g b]]
-  (format "rgb(%f,%f,%f)" r g b))
+(defn color
+  ([[r g b]]
+     (format "rgb(%f,%f,%f)" r g b))
+
+  ([[r g b] a]
+     (format "rgba(%f,%f,%f,%f)" r g b a)))
 
 (defn filled-rect [ctx [x y] [w h] color]
-  (set! (.-fillStyle ctx) color)
-  (.fillRect ctx x y w h))
+  (let [[tx ty tw th] (transform [x y w h])]
+    (set! (.-fillStyle ctx) color)
+    (.fillRect ctx tx ty tw th)))
+
+(defn draw-sprite [ctx img [x y]]
+  (let [[tx ty tw th] (transform [x y 1 1])
+        [sw sh] (img-dims img)]
+    (.drawImage ctx img
+                0 0 sw sh
+                tx ty tw th)))
+
+(defn get-img [url]
+  (let [img (js/Image.)]
+    (set! (.-src img) url)
+    img))
 
 (defn with-img [url callback]
-  (let [img (js/Image.)]
+  (let [img (get-img url)]
     (set! (.-onload img) (fn [] (callback img)))
-    (set! (.-src img) url)
     img))
 
 (defn img-dims [img]
   [(.-width img) (.-height img)])
 
+(defn img->canvas [img]
+  (let [[w h] (img-dims img)
+        canvas (make-canvas [w h])]
+    (.drawImage (context canvas) img 0 0)
+    canvas))
+
 (defn get-pixel-data [img]
-  (let [canvas (dom/createDom "canvas")
+  (let [canvas (img->canvas img)
         [w h] (img-dims img)]
-    (set! (.-width canvas) w)
-    (set! (.-height canvas) h)
-    (doto (context canvas)
-      (.drawImage img 0 0))
     {:data (-> (context canvas) (.getImageData 0 0 w h) (.-data))
      :dims [w h]}))
 
@@ -95,8 +122,7 @@ space taking into account the current viewport"
   (let [data (:data pdata)
         r (aget data idx)
         g (aget data (+ idx 1))
-         b (aget data (+ idx 2))
-        a (aget data (+ idx 3))]
+        b (aget data (+ idx 2))]
     [r g b]))
 
 (defn get-pixel [pdata x y]
@@ -106,50 +132,76 @@ space taking into account the current viewport"
       (get-pixel-idx pdata idx)
       *default-color*)))
 
+(defn get-pixel-alpha [pdata x y]
+  (let [[w h] (:dims pdata)
+        data (:data pdata)
+        idx (+ (* x 4) (* w y 4))]
+    (aget data (+ idx 3))))
+
+(defn resize-nearest-neighbor [img dims]
+  (let [[nw nh] dims
+        pdata (get-pixel-data img)
+        [iw ih] (:dims pdata)
+        rw (/ nw iw)
+        rh (/ nh ih)
+        canvas (make-canvas dims)
+        ctx (context canvas)]
+    
+    (doseq [x (range iw)
+            y (range ih)]
+      (let [pix (get-pixel pdata x y)]
+        (set! (.-fillStyle ctx) (color pix (get-pixel-alpha pdata x y)))
+        (.fillRect ctx (* x rw) (* y rh) rw rh)))
+
+    canvas))
+
+(def +map-symbols+ nil) ;; with-prepare-assets will fill this in
+
 (defn load-map [img]
-  {:pdata (get-pixel-data img)
-   :dims (img-dims img)})
+  (let [[w h] (img-dims img)
+        dims [w h]
+        pdata (get-pixel-data img)
+        data (into
+           []
+           (for [idx (range (* w h))]
+             (+map-symbols+ (get-pixel-idx pdata (* 4 idx)))))]
+    
+   {:pdata pdata
+    :dims dims
+    :data data}))
 
-(def +map-symbols+
-  {[255 255 255] {:kind :image
-                  :image (with-img "sprites/air.png" identity)}
-   [255 0     0] {:kind :image
-                  :image (with-img "sprites/dirt.png" identity)}
-   [0   0   255] {:kind :rect
-                  :color [0 0 255]}})
-
-(defn draw-sprite [ctx img pos]
-  (let [[tx ty tw th] (transform [(nth pos 0) (nth pos 1) 1 1])
-        [sw sh] *tile-dims*]
-    (.drawImage ctx img
-                0 0 sw sh
-                tx ty tw th)))
+(defn get-map-data [map x y]
+  (let [[w h] (:dims map)
+        data (:data map)]
+    (when (and (>= x 0) (>= y 0) (< x w) (< y h))
+      (nth data (+ x (* y w))))))
 
 (defn draw-map [map]
-  (let [pdata (:pdata map)
-        [w h] (:dims map)
-        [vw vh] (viewport-dims)
-        [vx vy] (viewport-offset)
+  (let [[vx vy vw vh] *viewport*
         ctx (context)]
 
     (doseq [x (range vw)
             y (range vh)]
       (let [rx (+ x vx)
             ry (+ y vy)
-            [tx ty tw th] (transform [rx ry 1 1])
             [sw sh] *tile-dims*
-            pix (get-pixel pdata (Math/floor rx) (Math/floor ry))
-            rec (+map-symbols+ pix)]
+            frx (Math/round rx)
+            fry (Math/round ry)
+            drx (- frx rx)
+            dry (- fry ry)
+            rec (get-map-data map frx fry)
+            sx (+ rx drx)
+            sy (+ ry dry)]
         (cond
           (nil? rec)
-          (filled-rect ctx [tx ty] [tw th] (color *default-color*))
+          (filled-rect ctx [sx sy] [1 1] (color *default-color*))
           
           (= (rec :kind) :rect)
-          (filled-rect ctx [tx ty] [tw th]
+          (filled-rect ctx [sx sy] [1 1]
                        (color (rec :color)))
 
           (= (rec :kind) :image)
-          (draw-sprite ctx (:image rec) [rx ry]))))))
+          (draw-sprite ctx (:image rec) [sx sy]))))))
 
 (def *current-map* nil)
 
@@ -178,12 +230,20 @@ space taking into account the current viewport"
 (def +ticks-per-ms+ (/ 30 1000))
 (def +secs-per-tick+ (/ 30))
 (def *guy-position* [1 1])
-(def *guy-sprite* (with-img "sprites/guy.png" identity))
+(def *guy-sprite* (get-img "sprites/guy.png"))
+(def *hud-sprite* (get-img "hud/hud.png"))
 
+(defn draw-hud []
+  (let [[w h] (img-dims *hud-sprite*)
+        [sw sh] *world-dims*]
+    (.drawImage (context) *hud-sprite*
+                0 0 w h
+                0 0 sw sh)))
 (defn draw []
   (clear)
   (draw-map *current-map*)
-  (draw-sprite (context) *guy-sprite* *guy-position*))
+  (draw-sprite (context) *guy-sprite* *guy-position*)
+  (draw-hud))
 
 (defn cycle []
   (let [now (js/goog.now)
@@ -237,10 +297,10 @@ space taking into account the current viewport"
       (set! *guy-position* [x (+ y step)]))))
 
 (def *viewport-velocity* [0 0])
-(def +viewport-spring-constant+ 1)
+(def +viewport-spring-constant+ 3)
 (def +viewport-drag-coefficient+ 1)
 (def +viewport-mass+ 1)
-(def +viewport-max-displacement+ 3)
+(def +viewport-max-displacement+ 1)
 
 (defn vec-add [[ax ay] [bx by]]
   [(+ ax bx) (+ ay by)])
@@ -294,14 +354,46 @@ space taking into account the current viewport"
   (update-guy-keyboard)
   (update-viewport-position))
 
+(defn with-prepared-assets [callback]
+  ;; a few assets we can resize lazily
+  (with-img "hud/hud.png"
+    (fn [hud]
+      (set! *hud-sprite* (resize-nearest-neighbor hud *world-dims*))))
+
+  (with-img "sprites/guy.png"
+    (fn [guy]
+      (set! *guy-sprite* (resize-nearest-neighbor guy *tile-in-world-dims*))))
+  
+  ;; its critical that +map-symbols+ be built before callback is
+  ;; invoked
+  (with-img "sprites/air.png"
+    (fn [air]
+      (with-img "sprites/dirt.png"
+        (fn [dirt]
+          (set! +map-symbols+
+                {[255 255 255]
+                 {:kind :image
+                  :image (resize-nearest-neighbor air *tile-in-world-dims*)}
+                 
+                 [255 0     0]
+                     {:kind :image
+                      :image (resize-nearest-neighbor dirt *tile-in-world-dims*)}
+                     
+                     [0   0   255]
+                     {:kind :rect
+                      :color [0 0 255]}})
+          (callback))))))
+
 (defn ^:export main []
   (dom/setTextContent (content) "")
   (prepare-display)
   (prepare-input)
   
-  (with-img "maps/test.png"
-    (fn [img]
-      (set! *current-map* (load-map img))
-      (until-false cycle (/ +ticks-per-ms+)))))
+  (with-prepared-assets
+    (fn []
+      (with-img "maps/test.png"
+        (fn [img]
+          (set! *current-map* (load-map img))
+          (until-false cycle (/ +ticks-per-ms+)))))))
 
 
